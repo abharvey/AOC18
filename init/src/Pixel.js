ec5aab4aa59baab6a1a81fc4e9dd6dead4ed517c
abharvey <alex@alexharvey.tech>
2018-12-11 19:10:13 -0500

    now just maths

--- init/src/Pixel.js
+++ init/src/Pixel.js
@@ -1,9 +1,21 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 import styled, { keyframes } from "styled-components";
 
-const pixel = keyframes`
+const pixel = props => keyframes`
     0% { left: 800px; top: 500px; }
-    100% { left: -100px; top: 200px;}
+    100% { 
+        left: -100px; top: 200px;
+    }
+`;
+
+const computeDist = (pos, vel) => {};
+
+const pixelTemp = props => keyframes`
+    0% { left: ${props.xPos}px; top: ${props.yPos}px; }
+    100% { 
+        left: ${computeDist(props.xPos, props.xVel)}px; 
+        top: ${computeDist(props.xPos, props.xVel)}px;
+    }
 `;
 
 const Pixel = styled.div`
@@ -12,7 +24,7 @@ const Pixel = styled.div`
   width: 10px;
   height: 10px;
   background: purple;
-  animation: ${pixel} 3s linear infinite;
+  animation: ${props => pixel(props)} 3s linear infinite;
 `;
 
 export default Pixel;
